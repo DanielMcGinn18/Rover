51fca76005eca68de7793c6201d8c827236a25e3
Daniel McGinn <danmcginn@comcast.net>
2021-02-25 00:58:42 -0500

    Fix Formatting

--- static/js/main.js
+++ static/js/main.js
@@ -10,8 +10,8 @@ function requestData()
 	            /*var x = JSON.stringify(requests);
 	            document.getElementById("sensor1").innerHTML = x*/
 	            var x = requests.responseJSON
-	            document.getElementById("sensor1").innerHTML = '<i class="fas fa-satellite-dish"></i>' + x[1]
-	            document.getElementById("sensor2").innerHTML = '<i class="fab fa-raspberry-pi"></i>' + x[2]
+	            document.getElementById("sensor1").innerHTML = x[1]
+	            document.getElementById("sensor2").innerHTML = x[2]
 	            setTimeout(requestData, 1000);
 	        });
         }
