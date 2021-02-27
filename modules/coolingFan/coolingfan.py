52466bac531068e63be14c9812cf2abfcb206cca
Daniel McGinn <danmcginn@comcast.net>
2021-02-27 12:01:50 -0500

    Turn off on KeyboardInterrupt

--- modules/coolingFan/coolingfan.py
+++ modules/coolingFan/coolingfan.py
@@ -17,37 +17,41 @@ s.connect(("8.8.8.8",80))
 ip_address = s.getsockname()[0] # Get IP Address
 s.close()
 
-#rendering the HTML page which has the button
-@app.route('/')
-def index():
-	return render_template('coolingfan.html')
+try:
+	#rendering the HTML page which has the button
+	@app.route('/')
+	def index():
+		return render_template('coolingfan.html')
 
-#background process happening without any refreshing
-@app.route('/background_process')
-def background_process():
-	global fan
-	if fan == 0:
-		fan = 1
-		GPIO.output(16, fan) # switch it on
-		print ("Fan On")
-	else:
-		fan = 0
-		GPIO.output(16, fan) # switch it off
-		print ("Fan Off")
-	return ("nothing")
+	#background process happening without any refreshing
+	@app.route('/background_process')
+	def background_process():
+		global fan
+		if fan == 0:
+			fan = 1
+			GPIO.output(16, fan) # turn fan on
+			print ("Fan On")
+		else:
+			fan = 0
+			GPIO.output(16, fan) # turn fan off
+			print ("Fan Off")
+		return ("nothing")
 
-@app.route('/data', methods=["GET", "POST"])
-def data():
-	global fan
-	if fan == 1:
-		string = 'Cooling Fan: On'
-	else:
-		string = 'Cooling Fan: Off'
-	data = [time() * 1000, string]
-	response = make_response(json.dumps(data))
-	response.content_type = 'application/json'
-	return response
+	@app.route('/data', methods=["GET", "POST"])
+	def data():
+		global fan
+		if fan == 1:
+			string = 'Cooling Fan: On'
+		else:
+			string = 'Cooling Fan: Off'
+		data = [time() * 1000, string]
+		response = make_response(json.dumps(data))
+		response.content_type = 'application/json'
+		return response
 
-if __name__ == '__main__':
+	if __name__ == '__main__':
 
-	app.run(host=ip_address, port=8000, debug=False)
+		app.run(host=ip_address, port=8000, debug=False)
+except KeyboardInterrupt:
+	fan = 0 # turn fan off
+    print("\n-------------------EXIT-------------------")
