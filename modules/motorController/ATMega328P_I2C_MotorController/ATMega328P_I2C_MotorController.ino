2396ecc01603119ea3cb2636ab9067242c247a73
Daniel McGinn <danmcginn@comcast.net>
2021-06-26 16:32:03 -0400

    Update Motor Control Module

--- modules/motorController/ATMega328P_I2C_MotorController/ATMega328P_I2C_MotorController.ino
+++ modules/motorController/ATMega328P_I2C_MotorController/ATMega328P_I2C_MotorController.ino
@@ -6,68 +6,108 @@
 // Include the Wire library for I2C
 #include <Wire.h>
  
-// LED on pin 13
-const int ledPin = 13; 
+#define LED_PIN 13
+boolean ledon = HIGH;
+// initialize the library with the numbers of the interface pins
+
+byte slave_address = 7;
 
 //motor A connected between A01 and A02
+//motor B connected between B01 and B02
 
 int STBY = 4; //standby
-int x, Motor, Direction, Speed; // initilize variable
+int x, motorA, directionA, speedA, motorB, directionB, speedB; // initilize variable
 
 //Motor A
 int PWMA = 5; //Speed control
 int AIN2 = 2; //Direction
 int AIN1 = 6; //Direction
+
+//Motor B
+int PWMB = 9; //Speed control
+int BIN1 = 7; //Direction
+int BIN2 = 8; //Direction
  
 void setup() {
-  // Join I2C bus as slave with address 8
-  Wire.begin(0x8);
+  pinMode(STBY, OUTPUT);
+  
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
 
-  // Setup Serial Monitor
-  Serial.begin(9600);
+  pinMode(PWMB, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
 
-pinMode(STBY, OUTPUT);
-pinMode(PWMA, OUTPUT);
-pinMode(AIN1, OUTPUT);
-pinMode(AIN2, OUTPUT);
-
-  // Call receiveEvent when data received                
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+  
+  Wire.begin(slave_address);
   Wire.onReceive(receiveEvent);
   
-  // Setup pin 13 as output and turn LED off
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  Serial.begin(9600); // Setup Serial Monitor
 }
  
-// Function that executes whenever data is received from master
+void loop() {
+
+}
+
 void receiveEvent(int howMany) {
-  while (Wire.available()) { // loop through all but the last
-    char c = Wire.read(); // receive byte as a character
-    digitalWrite(ledPin, c);
-    Direction = 1; // Forward
-    Speed = c*2; // Speed
-    Motor = 1;
-  
-  if(Speed > 0){
-  move(Motor, Speed, Direction); //motor 1
+  int numOfBytes = Wire.available();
+  //display number of bytes and cmd received, as bytes
+  Serial.print("numOfBytes:");
+  Serial.println(numOfBytes);
+
+  int dataArray[numOfBytes];
+
+  byte b = Wire.read();  //cmd
+  Serial.print("cmd:");
+  Serial.println(b);
+
+
+  //display message received, as char
+  for(int i=0; i<numOfBytes-1; i++){
+    char data = Wire.read();
+    dataArray[i] = int(data);
+    Serial.print("data:");
+    Serial.println(int(data));
+  }
+  motorA = dataArray[0];
+  directionA = dataArray[1];
+  speedA = dataArray[2]*2;
+
+  motorB = dataArray[3];
+  directionB = dataArray[4];
+  speedB = dataArray[5]*2;
+
+  if (speedA > 0 || speedB > 0) {
+  move(motorA, speedA, directionA); //motor A
+  move(motorB, speedB, directionB); //motor B
   }else{
   stop();
   }
+  toggleLED();
+}
+
+void toggleLED(){
+  ledon = !ledon;
+  if(ledon){
+    digitalWrite(LED_PIN, HIGH);
+  }else{
+    digitalWrite(LED_PIN, LOW);
   }
 }
-void loop() {
-  delay(200);
-}
+
 
 void move(int motor, int motorSpeed, int motorDirection){
 //speed: 0 is off, and 255 is full speed
-//direction: 0 clockwise, 1 counter-clockwise
+//direction: 0 backward, 1 forward
   Serial.print("motor:");
   Serial.print(motor);
-  Serial.print("     motorSpeed:");
-  Serial.print(motorSpeed);
   Serial.print("     motorDirection:");
-  Serial.println(motorDirection);
+  Serial.print(motorDirection);
+  Serial.print("     motorSpeed:");
+  Serial.println(motorSpeed);
 
 digitalWrite(STBY, HIGH); //disable standby
 
@@ -83,10 +123,15 @@ if(motor == 1){
 digitalWrite(AIN1, inPin1);
 digitalWrite(AIN2, inPin2);
 analogWrite(PWMA, motorSpeed);
+}else{
+digitalWrite(BIN1, inPin1);
+digitalWrite(BIN2, inPin2);
+analogWrite(PWMB, motorSpeed);
 }
 }
 
 void stop(){
 //enable standby
 digitalWrite(STBY, LOW);
+Serial.println("STOP");
 }
--- modules/motorController/README.md
+++ modules/motorController/README.md
@@ -1,15 +1,14 @@
-## Library dependencies
+## Motor Control Module
 Install the following dependencies
 
 ```
 sudo apt-get install python3-smbus
-
 ```
 
 Hookup for the Arduino and Raspberry Pi
 
 |  | Raspberry Pi | Arduino Uno | 
 | :---: | :---: | :---: | 
-| GND | Pin 9 | GND -> ATMega328P Pin 22  |
-| SDA | Pin 3 | A4 -> ATMega328P Pin 27  |
-| SCL | Pin 5 | A5 -> ATMega328P Pin 28 |
+| GND | Pin 9 | GND (ATMega328P Pin 22)  |
+| SDA | Pin 3 | A4 (ATMega328P Pin 27)  |
+| SCL | Pin 5 | A5 (ATMega328P Pin 28) |
