2b2bfe4c5f45a714041dde2f4537287b63369488
Daniel McGinn <danmcginn@comcast.net>
2021-06-27 16:22:31 -0400

    Update modules in main.py

--- main.py
+++ main.py
@@ -3,15 +3,26 @@
 from flask import Flask,render_template,url_for,request,redirect, make_response, Response, request # micro web framework 
 from camera import VideoCamera # Camera Module
 import RPi.GPIO as GPIO # GPIO library to control GPIO pins of Raspberry Pi
-GPIO.setmode(GPIO.BCM)
-# from smbus2 import SMBus # i2c capabilities to control atmega328p
 import time, threading, os, socket, random, json
 from time import time
 from random import random
 import board, adafruit_hcsr04 # Libraries for Ultrasonic Sensor
 
-GPIO.setup(16, GPIO.OUT) # GPIO Pin for Cooling Fan
-GPIO.output(16, 0) # Initialize Fan off
+import smbus
+
+speed = 20
+direction = 'Stop'
+
+bus = smbus.SMBus(1)
+
+# I2C address of Arduino Slave
+i2c_address = 0x07
+i2c_cmd = 0x01
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.OUT) 
+fan = 0
+GPIO.output(16, fan) # turn fan off
 
 pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
 
@@ -25,12 +36,44 @@ s.close()
 
 host_port = 8000 # Set host port
 
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6) # Setup Untrasonic Sensor
+def Forward(speed):
+    global direction
+    print('Forward')
+    speed = int(speed)/2
+    data = [0, 1, int(speed), 1, 1, int(speed)]
+    bus.write_i2c_block_data(i2c_address, i2c_cmd, data)
+    direction = 'Forward'
 
-# Initialize Variables for I2C Communication 
-# addr = 0x8 # bus address
-# bus = SMBus(1) # indicates /dev/ic2-1
-# numb = 1
+def Left(speed):
+    global direction
+    speed = int(speed)/2
+    print('Left')
+    data = [0, 0, int(speed), 1, 1, int(speed)]
+    bus.write_i2c_block_data(i2c_address, i2c_cmd, data)
+    direction = 'Left'
+
+def Right(speed):
+    global direction
+    speed = int(speed)/2
+    print('Right')
+    data = [0, 1, int(speed), 1, 0, int(speed)]
+    bus.write_i2c_block_data(i2c_address, i2c_cmd, data)
+    direction = 'Right'
+
+def Backward(speed):
+    global direction
+    speed = int(speed)/2
+    print('Backward')
+    data = [0, 0, int(speed), 1, 0, int(speed)]
+    bus.write_i2c_block_data(i2c_address, i2c_cmd, data)
+    direction = 'Backward'
+
+def Stop():
+    global direction
+    print('Stop')
+    data = [0, 1, 0, 1, 1, 0]
+    bus.write_i2c_block_data(i2c_address, i2c_cmd, data)
+    direction = 'Stop'
 
 @app.route('/')
 def index():
@@ -48,30 +91,58 @@ def video_feed():
     return Response(gen(pi_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route("/test", methods=["POST"])
-def test():
-    if form.validate_on_submit():
-        print(request.form)
-        if 'Fan' in request.form:
-            GPIO.output(16, 1)
-    slider1 = request.form["slider1"]  # Get slider Value
-    p.ChangeDutyCycle(float(slider1)) # Change duty cycle
-    sleep(1)
-    p.ChangeDutyCycle(0)
-    return render_template_string(TPL)
+@app.route("/", methods=['GET', 'POST'])
+def submit(): 
+    global fan, speed, direction
+    if request.method == "POST":
+        if request.form.get("fan"):
+            if fan == 0:
+                fan = 1
+                GPIO.output(16, fan) # turn fan on
+                print ("Fan On")
+            else:
+                fan = 0
+                GPIO.output(16, fan) # turn fan off
+                print ("Fan Off")
+        if request.form.get("Fwd"):
+            Forward(speed)
+        elif request.form.get("Left"):
+            Left(speed)
+        elif request.form.get("Right"):
+            Right(speed)
+        elif request.form.get("Bwd"):
+            Backward(speed)
+        elif request.form.get("Stop"):
+            Stop()
+        elif request.form.get("Speed"):
+            speed = request.form.get("Speed")
+            print(request.form.get("Speed"))
+            if direction == 'Forward':
+                Forward(speed)
+            elif direction == 'Left':
+                Left(speed)
+            elif direction == 'Right':
+                Right(speed)
+            elif direction == 'Backward':
+                Backward(speed)
+            else:
+                Stop()
+    return render_template('RoverController.html')
 
 # Send sensor values to webserver
 @app.route('/data', methods=["GET", "POST"])
 def data():
-    # Data1 = random() * 100 # Random Number for testing
-    try:
-        distance = 'Ultrasonic Sensor: ' + str(round(sonar.distance,2)) + ' cm'
-    except RuntimeError:
-        distance = 'Error'
+    global speed, fan, direction
+    directionData = direction
+    speedData = speed
+    if fan == 1:
+        fanData = 'Cooling Fan: On'
+    else:
+        fanData = 'Cooling Fan: Off'
     temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
     temp_US = ('CPU Temp: ' + str(round(((float(temp.split('=')[1].split("'")[0])*1.8)+30),2)) +
      u'\N{DEGREE SIGN}' + ' F') # Convert to Fahrenheit
-    data = [time() * 1000, distance, temp_US]
+    data = [time() * 1000, temp_US, speedData, fanData, directionData]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
